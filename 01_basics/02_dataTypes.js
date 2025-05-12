f8757864bf6ff8d809dca464fb1dc09cc9f9f8a5
umerfarooque00786 <umerfarooque00786@gmail.com>
2025-05-12 22:25:09 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strick"; //treat all js code newer version
+
+// alert( 3 + 3 ) // we are using node js not browser
+
+let name ="umer"
+let age = 25
+let loggedIn = false
+let set;
+
+
+// Number => 2 to power 53
+// Bignit
+// String => ""
+// boolean => true/false
+// null => standalone value
+// Undefined =>
+// Symbol => unique
+
+
+// object
+
+
+console.log(typeof  undefined); //undefined
+console.log (typeof null); //Object
